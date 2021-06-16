f4c38e71dc58200a77a3076f779438020ac4504c
lang <z954857159@sina.com>
2021-06-16 20:00:45 +0800

    添加

---
+++ springcloudpro/src/main/java/com/lang/springcloudpro/SpringcloudproApplication.java
@@ -0,0 +1,13 @@
+package com.lang.springcloudpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudproApplication.class, args);
+    }
+
+}
---
+++ springcloudpro/src/test/java/com/lang/springcloudpro/SpringcloudproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lang.springcloudpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
